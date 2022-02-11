45fee4a1741a1867855cb8f985330929fabd69f7
Mykola <kaljandlux@gmail.com>
2022-02-11 21:16:31 +0100

    borderless refactoring

--- server/model/__init__.py
+++ server/model/__init__.py
@@ -1,2 +1,4 @@
 from .neural_net import NeuralNet
+from .borderless_recognizer import BorderlessTableCellRecognizer
+from .split_table_recognizer import SplitTableRecognizer, SplitTableModel, SplitTableHeuristic
 from .ocr import OCR
--- server/model/pipeline.py
+++ server/model/pipeline.py
@@ -1,6 +1,12 @@
 from server.model.split_table_recognizer import SplitTableRecognizer
 from ..service import Document
-from . import NeuralNet, OCR
+from . import (NeuralNet, 
+               OCR, 
+               BorderlessTableCellRecognizer,
+               BorderedTableCellRecognizer,
+               SplitTableRecognizer, 
+               SplitTableModel, 
+               SplitTableHeuristic)
 
 
 class CascadeTabNetPipeline:
@@ -10,14 +16,34 @@ class CascadeTabNetPipeline:
         self._bordered_table_cell_recognizer = BorderedTableCellRecognizer()
         self._borderless_table_cell_recognizer = BorderlessTableCellRecognizer()
 
-        self._split_table_recognizer: SplitTableRecognizer = None  # ??
+        self._split_table_recognizer: SplitTableRecognizer = None
 
         self._ocr: OCR = None
 
-    def configure(self, config: dict):
-        pass
+    def configure(self, config: dict) -> dict:
+        if config['splitted_table_recognition'] == 'heuristic':
+            self._split_table_recognizer = SplitTableHeuristic()
+        else:
+            self._split_table_recognizer = SplitTableModel()
 
-    def predict(self, document: Document, model_configuration: dict):
-        self.configure(model_configuration)
+        self._ocr = OCR(config['ocr'], config['lang'])
 
-        self._neural_net.predict(document)
+    def flush(self):
+        self._split_table_recognizer = None
+        self._ocr = None
+
+    def predict(self, document: Document, config: dict) -> Document:
+        self.configure(config)
+
+        self._neural_net.predict(document, threshold=config['threshold'])
+
+        self._bordered_table_cell_recognizer
+        self._borderless_table_cell_recognizer.recognize_borderless(document)
+
+        self._split_table_recognizer.process(document)
+
+        self._ocr.process(document)
+
+        self.flush()
+
+        return document
