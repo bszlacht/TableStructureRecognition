7fe4590c9c67123102386fcb84f2c43b422c12d7
Mykola <kaljandlux@gmail.com>
2022-02-11 20:27:49 +0100

    data.py refactoring

--- server/model/pipeline.py
+++ server/model/pipeline.py
@@ -1,3 +1,23 @@
+from server.model.split_table_recognizer import SplitTableRecognizer
+from ..service import Document
+from . import NeuralNet, OCR
+
+
 class CascadeTabNetPipeline:
     def __init__(self) -> None:
+        self._neural_net = NeuralNet()
+        
+        self._bordered_table_cell_recognizer = BorderedTableCellRecognizer()
+        self._borderless_table_cell_recognizer = BorderlessTableCellRecognizer()
+
+        self._split_table_recognizer: SplitTableRecognizer = None  # ??
+
+        self._ocr: OCR = None
+
+    def configure(self, config: dict):
         pass
+
+    def predict(self, document: Document, model_configuration: dict):
+        self.configure(model_configuration)
+
+        self._neural_net.predict(document)
